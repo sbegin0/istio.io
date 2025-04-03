fa2fd1128aa120c2fc89ea4ae9eb593f6b4aef17
sbegin0 <sam.begin9@gmail.com>
2025-04-03 17:04:48 -0400

    deleted latest out of ts for edge function

--- netlify/edge-functions/redirect-to-latest.ts
+++ netlify/edge-functions/redirect-to-latest.ts
@@ -5,7 +5,7 @@ export default async (request: Request) => {
   // Excludes paths containing "/docs/"
   if (url.pathname.match(/^\/v\d+(\.\d+)*/) && !url.pathname.includes('/docs/')) {
     // Preserve the rest of the path after the version part
-    const newPath = url.pathname.replace(/^\/v\d+(\.\d+)*/, '/latest');
+    const newPath = url.pathname.replace(/^\/v\d+(\.\d+)*/, '');
     
     return Response.redirect(new URL(newPath, url.origin), 301);
   }
